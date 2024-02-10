4174062847d871b2a4e7f931c1a2348f2d109f29
Samuel Relling <srelling@student.ethz.ch>
2024-02-10 16:34:45 +0100

    removed trash

--- README.mk
+++
@@ -1,8 +0,0 @@
-This is a simple Pawn Chess Project where we write a chess engine for only pawns.
-
-##To build:
-
-1) mkdir build
-2) cd build
-3) cmake ..
-4) make pawn_chess
--- build/CMakeFiles/pawn_chess.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pawn_chess.
-# This may be replaced when dependencies are built.
